86d325fae60a09688306277f5579dcf4aaebbf98
Kevin Arias <kevinlh1337@gmail.com>
2019-11-30 16:10:49 -0500

    Se sube el proyecto a github.

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Software de inventario.
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <!--VERIFICADO-->
+    <body>
+        <div id="pagewrap">
+        <div id="header">
+            <div id="logo1">
+                <img class="logo1" src="css/imagenes/logo.png">
+            </div><!--LOGO1-->
+            <div id="logo2">
+                <img class="logo2" src="css/imagenes/logo.png">
+            </div><!--LOGO2-->
+            <div id="info">
+                <h1>Software de Inventario</h1>
+                <p class="subtitulo">Aquí encontrarás toda la información acerca de los Software de inventario.</p>
+            </div><!--INFO-->
+        </div><!--HEADER-->
+        <div id="menu">
+            <p class="inicio"><b>Inicio</b></p>
+            <p class="mision"><b>Misión</b></p>
+            <p class="vision"><b>Visión</b></p>
+            <p class="contactenos"><b>Contactenos</b></p>
+        </div><!--MENU-->
+        <div id="content">
+            <h2>¿Qué es un Software de Inventario?</h2>
+            <p>
+                Los software de inventarios o software de control de inventarios son programas creados para facilitar la 
+                gestión del inventario de una empresa, entendido este como el conjunto de elementos que forman su patrimonio. 
+                Así, pueden utilizarlo negocios de cualquier sector, sin importar su tamaño, aunque es especialmente interesante 
+                para aquellos dedicados a la compraventa de existencias. Permiten saber la cantidad disponible de existencias, su 
+                localización y las entradas y salidas de mercancías.
+            </p>
+            <p>
+                Todo ello contribuye a una mejor planificación, una mayor productividad y a la satisfacción del cliente.
+                Pongamos como ejemplo una librería: con este tipo de programas se podrá conocer en todo momento cuántos títulos y, 
+                a su vez, cuántos libros de cada título hay disponibles, tanto en almacén como en tienda. Asimismo, se puede obtener
+                una visión de los libros que se han vendido, descubriendo los preferidos por los clientes. De esta forma, 
+                se planifica con exactitud la adquisición de nuevas existencias para que siempre estén a disposición de los lectores.
+            </p>
+            <h2>
+                Tipos de Software de inventario:
+            </h2>
+            <p>
+                La oferta de programas de inventarios es amplia en el mercado español, aunque es importante tener en cuenta 
+                la variedad existente para escoger el programa que mejor se adapte a las características de la empresa. Así, se 
+                pueden distinguir programas:
+            </p>
+            <p class="tipo1">
+                <b>Especializados o partes de un software integral. </b>
+                Si bien se puede optar por un software de control de inventarios especializado, la mayoría se integran como 
+                parte de una solución mayor que incluye módulos de diferentes tareas de administración también esenciales en 
+                una empresa, tal es el caso de la contabilidad o la facturación.
+            </p>
+            <p class="tipo2">
+                <b>Gratuitos y de pago. </b>
+                Pensar en las características que debe reunir el software de inventarios es esencial a la hora de optar 
+                por una solución gratuita o de pago. Mientras las primeras suelen estar más limitadas en cuanto a funciones, 
+                los programas de pago ofrecen normalmente diferentes cuotas según las exigencias de cada usuario.
+            </p>
+            <p class="tipo3">
+                <b>Almacenamiento en la nube o local. </b>
+                Decidirse por una opción en la nube reporta numerosas ventajas, sobre todo, porque de esta forma el 
+                usuario puede acceder a los datos desde cualquier lugar y en cualquier momento. Sin embargo, se requiere 
+                Internet para su correcto funcionamiento. Los programas alojados en un dispositivo se instalan directamente 
+                en el ordenador que vaya a ser utilizado para la actividad.
+            </p>
+            <p class="tipo4">
+                <b>Licencia o pago por uso.</b> 
+                Hay programas que para usarlos requieren la adquisición de una licencia permanente con la que el 
+                usuario tendrá siempre acceso al programa. Sin embargo, la mayoría responde a un modelo de pago por 
+                uso que funciona por suscripción. De esta forma, los programas se pueden usar de acuerdo a las necesidades 
+                de la empresa, si bien suelen tener un costo económico 
+                <br>
+                mayor.
+            </p>
+        </div><!--CONTENIDO-->
+        <div id="sidebar1">
+            <h2>Contenido:</h2>
+            <p>1. Inicio</p>
+            <p>2. ¿Qué es un Software de Inventario?</p>
+            <p>3. Tipos de Software de Inventario</p>
+        </div> <!--SIDEBAR 1-->
+        <div id="sidebar2">
+            <h2>Nuestras Redes Sociales:</h2>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+        </div><!--SIDEBAR2-->
+        <div id="footer">
+                <img class="imagen2" src="css/imagenes/imagen1.png">
+                <img class="imagen3" src="css/imagenes/imagen2.png">
+            <h4>Contactenos</h4>
+            <p class="datos">            
+            Ingrese su nombre
+            <input type="text" maxlength="20" name="nombre">
+            <br>
+            
+
+            </br>           
+            Ingrese su correo electrónico
+            <input type="text" maxlength="20" name="correo">
+                <br>
+            </br>
+            <a class="aceptar" href="">Aceptar</a></p>
+
+
+        </div><!--FOOTER-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div><!--PAGEWRAP-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+    
+</html>
